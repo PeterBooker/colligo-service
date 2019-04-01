29b43beb40ed491deaecba4ef2ef99a0e643ea29
Peter Booker <mail@peterbooker.com>
2019-04-01 22:32:33 +0100

    Use new store package

--- internal/auth/twitter.go
+++ internal/auth/twitter.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/PeterBooker/colligo/internal/config"
+	"github.com/PeterBooker/colligo/internal/store"
 	"github.com/mrjones/oauth"
 )
 
@@ -17,7 +18,7 @@ var (
 )
 
 // TwitterGetToken ...
-func TwitterGetToken(c config.Config, w http.ResponseWriter, r *http.Request) error {
+func TwitterGetToken(c config.Config, s *store.Store, w http.ResponseWriter, r *http.Request) error {
 
 	consumer := oauth.NewConsumer(
 		c.Twitter.Key,
@@ -33,12 +34,12 @@ func TwitterGetToken(c config.Config, w http.ResponseWriter, r *http.Request) er
 	verifier := values.Get("oauth_verifier")
 	token := values.Get("oauth_token")
 
-	requestToken, found := tokens.Get(token)
+	visitor, found := s.Get(token)
 	if !found {
 		return errors.New("Token not found")
 	}
 
-	accessToken, err := consumer.AuthorizeToken(requestToken, verifier)
+	accessToken, err := consumer.AuthorizeToken(visitor.Token, verifier)
 	if err != nil {
 		return err
 	}
@@ -50,7 +51,14 @@ func TwitterGetToken(c config.Config, w http.ResponseWriter, r *http.Request) er
 }
 
 // TwitterSendUser ...
-func TwitterSendUser(c config.Config, w http.ResponseWriter, r *http.Request) (string, error) {
+func TwitterSendUser(c config.Config, s *store.Store, w http.ResponseWriter, r *http.Request) (string, error) {
+
+	values := r.URL.Query()
+	cb := values.Get("callback")
+
+	if cb == "" {
+		return "", errors.New("You must provide a callback URL")
+	}
 
 	consumer := oauth.NewConsumer(
 		c.Twitter.Key,
@@ -69,7 +77,12 @@ func TwitterSendUser(c config.Config, w http.ResponseWriter, r *http.Request) (s
 
 	log.Printf("Token: %s, loginURL: %s\n", requestToken, loginURL)
 
-	tokens.Set(requestToken.Token, requestToken)
+	v := &store.Visitor{
+		Token:    requestToken,
+		Callback: cb,
+	}
+
+	s.Set(requestToken.Token, v)
 
 	return loginURL, nil
 
